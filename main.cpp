86bbb35aef3c5aa066cd8569986f7035beda7f0a
Hope-Cube <117855454+Hope-Cube@users.noreply.github.com>
2022-11-24 15:05:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,56 @@
 #include <iomanip>
 using namespace std;
 
+class Point {
+    private:
+        int _x;
+        int _y;
+    public:
+        Point(int x, int y){
+            _x = x;
+            _y = y;
+        }
+        int X(){
+            return _x;
+        }
+        int Y(){
+            return _y;
+        }
+        string PointOut(){
+            string s = to_string(X()) + ", " + to_string(Y());
+            return s;
+        }
+};
+
+class LineGroup{
+    private:
+        vector<vector<Point>> lines;
+    public:
+        LineGroup(){
+        vector<vector<Point>> lines;
+        }
+        void Add(vector<Point> line){
+        lines.push_back(line);
+        }
+        string LinesOut(){
+        string o;
+        for(int i = 0; i < lines.size(); i++){
+                o = o + to_string(i+1) + "|\n";
+            for(int j = 0; j < lines[i].size(); j++){
+                o = o + lines[i][j].PointOut() + "\n";
+            }
+            o = o + "\n";
+        }
+        return o;
+        }
+};
+
 int Abs(int a)
 {
-    return a * ((a>0)-(a<0));
+    if(a < 0){
+        a = a * -1;
+    }
+    return a;
 }
 
 int Sign(int a)
@@ -28,27 +75,16 @@ int Sign(int a)
     }
 }
 
-int main()
-{
-    srand(time(0));
-    setlocale(LC_ALL,"");
-
-    char screen [32][32] = {};
-
+vector<Point> CreateLine(Point p1, Point p2){
     bool interchange = 0;
 
-    int x1 = 0;
-    int y1 = 0;
-    int x2 = -8;
-    int y2 = -4;
+    int x = p1.X();
+    int y = p1.Y();
 
-    int x = x1;
-    int y = y1;
-
-    int dx = Abs(x2-x1);
-    int dy = Abs(y2-y1);
-    int s1 = Sign(y2-y1);
-    int s2 = Sign(y2-y1);
+    int dx = Abs(p2.X()-p1.X());
+    int dy = Abs(p2.Y()-p1.Y());
+    int s1 = Sign(p2.X()-p1.X());
+    int s2 = Sign(p2.Y()-p1.Y());
 
     if(dy > dx){
         int t = dx;
@@ -65,6 +101,12 @@ int main()
     int A = 2 * dy;
     int B = 2 * dy - 2 * dx;
 
+    vector<Point> line;
+
+    Point p = Point(p1.X(),p1.Y());
+
+    line.push_back(p);
+
     for(int i = 1; i < dx; i++){
         if(error < 0){
             if(interchange == 1){
@@ -80,8 +122,33 @@ int main()
             x = x + s1;
             error = error + B;
         }
-        cout << x << " " << y << endl;
+        p = Point(x,y);
+        line.push_back(p);
     }
+
+    p = Point(p2.X(),p2.Y());
+    line.push_back(p);
+    return line;
+}
+
+int main()
+{
+    srand(time(0));
+    setlocale(LC_ALL,"");
+
+    vector<vector<char>> screen(32, vector<char> (32,'0'));
+
+    Point p1 = Point(0,0);
+    Point p2 = Point(8,4);
+
+    LineGroup lines = LineGroup();
+    lines.Add(CreateLine(p1,p2));
+
+    //p1 = Point(5,9);
+    //p2 = Point(8,20);
+
+    //lines.Add(CreateLine(p1,p2));
+    cout << lines.LinesOut();
 
     cout << "\n";
     system("pause");
